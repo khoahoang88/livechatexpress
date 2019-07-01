44be0b6ce8aecff96ee1ae4cc887e0e418d1f1b9
MAC <mac@MACs-MacBook-Pro.local>
2019-07-01 16:54:13 +0700

    change port

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 const app = express()
 const hogan = require('hogan-middleware')
 const bodyParser = require('body-parser')
 const request = require('request')
 const handlers = require('./modules/handlers')
 const messenger = require('./modules/messenger')
+app.set('port', process.env.PORT || port);
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'mustache')
 app.engine('mustache', hogan.__express)
@@ -21,4 +22,4 @@ app.get('/', (req, res) => {
 	console.log('2222: %i', handlers.startAgent('XXX', 'XXXX'))
 	console.log('RES %j', res)
 })
-app.listen(port, () => console.log(`Example app listening on port ${PORT}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
